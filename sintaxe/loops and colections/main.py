7953569000dbca36348af328e54fbb14176b8c38
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2024-01-22 08:21:47 -0300

    doc: Add loops and collections examples

--- sintaxe/loops and colections/main.py
+++ sintaxe/loops and colections/main.py
@@ -5,19 +5,27 @@
 for word in range(1, 6):
     print('Loading...')
 
+print('------------------')
+
 # Number list with range 1 to 5
 for item in range(1, 6):
     print(item)
 
+print('------------------')
+
 # Number list with range 20 to 40 and jump 2
 for item in range(20, 41, 2):
     print(item)
+
+print('------------------')
 
 # While loop
 counter = 0
 while counter < 10:
     print(counter)
     counter += 1
+
+print('------------------')
 
 # While loop with break
 counter = 76
@@ -27,13 +35,29 @@ while True:
     if counter == 80:
         break
 
+print('------------------')
+
 # Collections
 # Names list with 4 names
 names = ['João', 'Maria', 'José', 'Ana']
 for name in names:
     print(name)
 
+print('------------------')
+
 # Ages list with 3 ages
 ages = [33, 89, 27]
 for age in ages:
     print(age)
+
+print('------------------')
+
+# Mixed list with 3 names and 3 ages
+mixed = ['João', 33, 'Maria', 89, 'José', 27]
+for item in mixed:
+    print(item)
+
+print('------------------')
+# Aceessing list items by index
+print(mixed[0])
+print(mixed[1])
