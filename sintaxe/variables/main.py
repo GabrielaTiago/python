99c9734c5bb777d479d107fad037f152ccc757f0
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2024-01-21 22:01:56 -0300

    doc: Variables

---
+++ sintaxe/variables/main.py
@@ -0,0 +1,22 @@
+# Variables
+# Pattern: snake case
+
+# Numbers
+# Integers
+netwoork_velocity = 50
+print('netwoork_velocity: ', netwoork_velocity)
+
+# Float (Decimal)
+movie_grade = 8.5
+print('movie_grade: ', movie_grade)
+
+# Boolean (True, False)
+is_open = True
+is_closed = False
+print('is_open: ', is_open)
+print('is_closed: ', is_closed)
+
+# Strings (Single quotes or Double quotes)
+movie_name = "The Godfather"   # Double quotes
+user_name = 'John'             # Single quotes
+
