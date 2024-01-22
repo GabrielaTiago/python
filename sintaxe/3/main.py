0c0502192a86098931bd4a25bfcd1de233afb254
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2024-01-22 08:33:54 -0300

    Add solution for problem 3 - Sum of ages

---
+++ sintaxe/3/main.py
@@ -0,0 +1,13 @@
+# Problema 3 - Soma de idades
+# Escrvea um progrmama que dada uma lista de idades, retorna a soma das idades da coleção.
+
+# Entrada
+idades = [15,46,75,23,34]
+
+# Processamento
+soma = 0
+for idade in idades:
+    soma += idade
+
+# Saída
+print(f'A soma das idades é {soma}')
