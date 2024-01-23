43c0d1faedad6cca51a021263fd1ac408637b454
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2024-01-22 22:55:41 -0300

    doc: Update variables

--- sintaxe/variables/main.py
+++ sintaxe/variables/main.py
@@ -1,14 +1,18 @@
 # Variables
-# Pattern: snake case
+# Pattern:  lowercase, snake case
 
 # Numbers
 # Integers
 netwoork_velocity = 50
 print('netwoork_velocity: ', netwoork_velocity)
 
+print('------------------')
+
 # Float (Decimal)
 movie_grade = 8.5
 print('movie_grade: ', movie_grade)
+
+print('------------------')
 
 # Boolean (True, False)
 is_open = True
@@ -16,7 +20,39 @@ is_closed = False
 print('is_open: ', is_open)
 print('is_closed: ', is_closed)
 
+print('------------------')
+
 # Strings (Single quotes or Double quotes)
 movie_name = "The Godfather"   # Double quotes
 user_name = 'John'             # Single quotes
+print('movie_name: ', movie_name)
+print('user_name: ', user_name)
 
+print('------------------')
+
+# Multiple assignment (Unpacking)
+a1, a2, a3 = 1, 2, 3
+print('a1: ', a1)
+print('a2: ', a2)
+print('a3: ', a3)
+
+print('------------------')
+
+# Constants (Do not change)
+# Pattern: Uppercase, snake case
+PI = 3.14
+print('PI: ', PI)  # 3.14
+MAX_VELOCITY = 80
+print('MAX_VELOCITY: ', MAX_VELOCITY)  # 80
+
+print('------------------')
+
+# Dynamic typing
+v1 = 25.87
+v2 = True
+v3 = 'Gabriela'
+v4 = 15
+print('type(v1): ', type(v1)) # <class 'float'>
+print('type(v2): ', type(v2)) # <class 'bool'>
+print('type(v3): ', type(v3)) # <class 'str'>
+print('type(v4): ', type(v4)) # <class 'int'>
