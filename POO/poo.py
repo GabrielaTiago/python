9d89a134724f222acefcf059d88e87ce15f448c8
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2024-02-24 18:32:33 -0300

    Multilevel and Multiple Inheritance Concepts

--- POO/poo.py
+++ POO/poo.py
@@ -1,6 +1,6 @@
 # Classes
-# A class is a blueprint for creating objects (a particular data structure), providing initial values for state (member variables or attributes), and implementations of behavior (member functions or methods).
 
+# A class is a blueprint for creating objects (a particular data structure), providing initial values for state (member variables or attributes), and implementations of behavior (member functions or methods).
 # self parameter is a reference to the current instance of the class, and is used to access variables that belongs to the class.
 
 class Car: # class name
@@ -70,6 +70,7 @@ car2.info()
 print('-------------------')
 
 # Inheritance
+
 # Inheritance allows us to define a class that inherits all the methods and properties from another class.
 # Parent class is the class being inherited from, also called base class.
 # Child class is the class that inherits from another class, also called derived class.
@@ -95,3 +96,93 @@ class ElectricCar(Car):
 electric_car = ElectricCar('Blue', 'Tesla', 'Model S', 250, '100kWh')
 electric_car.info()
 electric_car.turn_on()
+
+print('-------------------')
+
+# Multilevel inheritance
+
+# When a class is derived from another derived class, it is called multilevel inheritance.
+# It is similar to single inheritance, but the difference occurs when a class is derived from another derived class.
+# The class is derived from another derived class.
+# Multilevel inheritance is bad for maintainability and readability, so it should be avoided. (heigth acoplament, height complexity, height risk of bugs, etc.)
+
+class SportsCar(Car):
+    def __init__(self, color, brand, model, top_speed, sport_package) -> None:
+        super().__init__(color, brand, model, top_speed)
+        self.sport_package = sport_package
+
+    def info(self):
+        super().info()
+        print(f'Sport package: {self.sport_package}')
+
+class SuperCar(SportsCar):
+    def __init__(self, color, brand, model, top_speed, sport_package, super_package) -> None:
+        super().__init__(color, brand, model, top_speed, sport_package)
+        self.super_package = super_package
+
+    def info(self):
+        super().info()
+        print(f'Super package: {self.super_package}')
+
+class HyperCar(SuperCar):
+    def __init__(self, color, brand, model, top_speed, sport_package, super_package, hyper_package) -> None:
+        super().__init__(color, brand, model, top_speed, sport_package, super_package)
+        self.hyper_package = hyper_package
+
+    def info(self):
+        super().info()
+        print(f'Hyper package: {self.hyper_package}')
+
+hyper_car = HyperCar('Yellow', 'Bugatti', 'Chiron', 420, 'Sport', 'Super', 'Hyper')
+hyper_car.info()
+
+print('-------------------')
+
+# Multiple inheritance
+
+# In multiple inheritance, the **features** of all the base classes are inherited into the derived class.
+# The class derived from multiple classes is called a derived class.
+
+class A:
+    def method(self):
+        print('Method A')
+
+class B:
+    def method(self):
+        print('Method B')
+
+class C(A, B):
+    pass
+
+c = C()
+c.method() # Method A (the method of the first class is called)
+print(C.mro()) # [<class '__main__.C'>, <class '__main__.A
+
+
+# Dunder methods
+
+# Dunder methods are special methods with double underscores at the beginning and end of their names.
+# They are also called magic methods.
+# They are used to create functionality that can't be represented as a normal method.
+# They are used to define the behavior of objects.
+# They are called automatically by Python interpreter.
+
+class Book:
+    def __init__(self, title, author, pages) -> None:
+        self.title = title
+        self.author = author
+        self.pages = pages
+
+    def __str__(self):
+        return f'{self.title} by {self.author}'
+
+    def __len__(self):
+        return self.pages
+
+    def __del__(self):
+        print('A book object has been deleted')
+
+book = Book('Clean Code', 'Robert C. Martin', 464)
+print(book) # Clean Code by Robert C. Martin
+print(len(book)) # 464
+del book # A book object has been deleted
