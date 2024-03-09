8958ac8bfbb7fa0082a59a2b24c9805d7291deb7
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2024-03-09 09:29:13 -0300

    Adds standard messages to the api response

---
+++ API/blog/default_messages.py
@@ -0,0 +1,11 @@
+AUTORS_NOT_FOUND_MESSAGE = 'Authors not found'
+AUTHOR_NOT_FOUND_MESSAGE = 'Author not found'
+AUTHOR_CREATED_MESSAGE = 'Author successfully created!'
+AUTHOR_UPDATED_MESSAGE = 'Author successfully updated!'
+AUTHOR_DELETED_MESSAGE = 'Author successfully deleted!'
+
+POSTS_NOT_FOUND_MESSAGE = 'Posts not found'
+POST_NOT_FOUND_MESSAGE = 'Post not found'
+POST_CREATED_MESSAGE = 'Post successfully created!'
+POST_UPDATED_MESSAGE = 'Post successfully updated!'
+POST_DELETED_MESSAGE = 'Post successfully deleted!'
